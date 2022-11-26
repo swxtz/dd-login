e07e588828b9492ed2dc636cef41abe22b4b80cc
Gustavo Mendonça <alf4r6@gmail.com>
2022-11-26 00:24:00 -0300

    feat: header form component

--- src/components/HeaderForm/HeaderForm.tsx
+++ src/components/HeaderForm/HeaderForm.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import loginSvg from "../../assets/svg/log-in.svg";
+import "./header-form.scss";
 
-export function Header () {
+export function HeaderForm() {
 	return (
 		<div className="header-wrapper">
 			<div className="header-title">
 				<img src={loginSvg} alt="Log-in" />
 				<h2>Faça seu login</h2>
+			</div>
+			<div className="header-subtitle">
+				<p>Entre com suas informações de cadastro.</p>
 			</div>
 		</div>
 	);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.scss";
 
 import { Logo } from "../../components/Logo/Logo";
+import { HeaderForm } from "../../components/HeaderForm/HeaderForm";
 
 export function Home() {
 	return (
@@ -9,6 +10,9 @@ export function Home() {
 			<div className="home-wrapper">
 				<div className="home-logo">
 					<Logo />
+				</div>
+				<div className="home-form">
+					<HeaderForm />
 				</div>
 			</div>
 		</div>
