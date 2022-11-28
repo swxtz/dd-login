7ef1d31f2d52d557d4010799c46b855b58470fb3
Gustavo Mendonça <alf4r6@gmail.com>
2022-11-27 22:36:37 -0300

    style: adding logic to the login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import { InputForm } from "../InputForm/InputForm";
 import "./login-form.scss";
 import { Checkbox } from "../Checkbox/Checkbox";
 import { ButtonLogin } from "../ButtonLogin/ButtonLogin";
 
 export function LoginForm() {
+
+	function handleSubmitForm(e:FormEvent) {
+		e.preventDefault();
+	}
+
 	return (
 		<div>
-			<form action="" className="login-form" method="post">
+			<form action="" onSubmit={(e) => {handleSubmitForm(e);}} className="login-form" method="post">
 				<InputForm
 					email="E-mail"
 					emailPlaceholder="Digite seu e-mail"
@@ -18,7 +23,7 @@ export function LoginForm() {
 					<Checkbox text="Lembre-me?" link="Esqueci minha senha" />
 				</div>
 				<div className="login-form-button">
-					<ButtonLogin  title="entrar" />
+					<ButtonLogin type="submit" title="entrar" />
 				</div>
 				<div className="login-form-link">
 					<p>Não tem uma conta? <a href="#">Registre-se</a></p>
